8253963123dbbc558036db55e0f16c26ae793c46
Abdul Rahmat <abdulrahmat97dev@gmail.com>
2025-11-28 14:38:16 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,7 +11,19 @@ const nextConfig = {
       'avatars.githubusercontent.com',
       'lh3.googleusercontent.com'
     ]
-  }
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
